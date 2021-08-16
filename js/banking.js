51bc0810cbf4aa1926c1149d5161fbeabc297d5e
shahriarAb <sm.abeed007@gmail.com>
2021-08-17 00:16:58 +0600

    add warning messages and customize code

--- js/banking.js
+++ js/banking.js
@@ -1,32 +1,50 @@
+function getInputValue(inputId) {
+    const input = document.getElementById(inputId);
+    const newAmountText = input.value;
+    const newAmount = parseFloat(newAmountText);
+    input.value = '';
+    return newAmount;
+}
+function getFieldValueAdd(fieldValue, amount) {
+    const totalFiledValue = document.getElementById(fieldValue);
+    const totalFieldValueText = totalFiledValue.innerText;
+    const previousAmount = parseFloat(totalFieldValueText);
+    totalFiledValue.innerText = previousAmount + amount;
+}
+function getCurrentBalance(){
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotalText = balanceTotal.innerText;
+    const previousBlanceTotal = parseFloat(previousBalanceTotalText);
+    return previousBlanceTotal;
+}
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBlanceTotal = getCurrentBalance();
+    if (isAdd == true) {
+        balanceTotal.innerText = previousBlanceTotal + amount;
+    }
+    else {
+        balanceTotal.innerText = previousBlanceTotal - amount;
+    }
+}
+
+
 document.getElementById('deposit-btn').addEventListener('click', function () {
-    //update deposit amount
-    const depositInput = document.getElementById('deposit-field');
-    const newDepositAmount = depositInput.value;
-    const depositTotal = document.getElementById('deposit-total');
-    const previousDepositAmount = depositTotal.innerText;
-    const updatedDepositTotal = parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
-    depositTotal.innerText = updatedDepositTotal;
-    depositInput.value = '';
-
-    //update balance amount
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBlanceTotal = balanceTotal.innerText;
-    const updatedBalanceTotal = parseFloat(previousBlanceTotal) + parseFloat(newDepositAmount);
-    balanceTotal.innerText = updatedBalanceTotal;
+    const newDepositAmount = getInputValue('deposit-field'); //get an convert input field value
+    if (newDepositAmount > 0) {
+        getFieldValueAdd('deposit-total', newDepositAmount); //update deposit
+        updateBalance(newDepositAmount, true); //update balance
+    }
 });
+
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    const withdrawInput = document.getElementById('withdraw-field');
-    const withdrawAmount = withdrawInput.value;
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const previousWithdrawAmount = withdrawTotal.innerText;
-    const updateWithdrawTotal = parseFloat(withdrawAmount) + parseFloat(previousWithdrawAmount);
-    withdrawTotal.innerText = updateWithdrawTotal;
-    withdrawInput.value = '';
-
-    //update balance amount
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalanceTotal = balanceTotal.innerText;
-    const updatedBalanceTotal = parseFloat(previousBalanceTotal) - parseFloat(withdrawAmount);
-    balanceTotal.innerText = updatedBalanceTotal;
-
+    const newWithdrawAmount = getInputValue('withdraw-field'); //get an convert input field value
+    const currentBalance = getCurrentBalance();
+    if (newWithdrawAmount > 0 && newWithdrawAmount <= currentBalance) {
+        getFieldValueAdd('withdraw-total', newWithdrawAmount); //update withdraw
+        updateBalance(newWithdrawAmount, false); //update balance
+    }
+    if(newWithdrawAmount > currentBalance){
+        document.getElementById('warning-message').innerText = 'Sorry! You can not withdraw more than you have.';
+    }
 });
--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-document.getElementById('login-button').addEventListener('click',function(){
+document.getElementById('login-button').addEventListener('click', function () {
     //get user email
     const emailField = document.getElementById('email-field');
     const userEmail = emailField.value;
@@ -6,11 +6,13 @@ document.getElementById('login-button').addEventListener('click',function(){
     const passwordField = document.getElementById('password-field');
     const userPassword = passwordField.value;
     //redirect to another page on checking email and password
-    if(userEmail == 'user@functionalbank.com' && userPassword == 'secret60'){
+    if (userEmail == 'user@functionalbank.com' && userPassword == 'secret60') {
         window.location.href = 'banking.html';
     }
-    else{
-        document.getElementById('section-1').style.backgroundColor = '#ff726f';
-        alert('Wrong email or password');
+    else {
+        document.getElementById('incorrect-message').innerText = 'Incorrect username or password';
+        document.getElementById('section-1').style.borderColor = 'red';
+        document.getElementById('email-field').style.borderColor = 'red';
+        document.getElementById('password-field').style.borderColor = 'red';
     }
 });
